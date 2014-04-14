bbc1e34cef0ab70ae32e539ff70ce7832c3ff321
Michael Schmitz <michael@schmitztech.com>
2014-04-14 16:31:27 -0700

    Small refactor.

--- tools/core/src/main/scala/org/allenai/aitk/parse/graph/TokenDependencyNode.scala
+++ tools/core/src/main/scala/org/allenai/aitk/parse/graph/TokenDependencyNode.scala
@@ -16,10 +16,12 @@ import org.allenai.aitk.lemmatize.Lemmatized
   * A representation for a node in the graph of dependencies.  A node
   * represents one or more adjacent tokens in the source sentence.
   */
-case class TokenDependencyNode(val id: Int, val token: Lemmatized[PostaggedToken]) {
-  def string = token.token.string
-  def postag = token.token.postag
-  def lemma = token.lemma
+case class TokenDependencyNode(val id: Int, val lemmatizedToken: Lemmatized[PostaggedToken]) {
+  def string = token.string
+  def postag = token.postag
+  def lemma = lemmatizedToken.lemma
+
+  def token: PostaggedToken = lemmatizedToken.token
 
   // extend Object
   override def toString() = s"$string-$id"
