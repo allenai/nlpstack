f0b254e5155a3c6c91a37c5657fff7e7f88c1b66
Michael Schmitz <michael@schmitztech.com>
2014-04-21 16:12:35 -0700

    Add examples for tools.

--- webapp/src/main/universal/public/js/tools.js
+++ webapp/src/main/universal/public/js/tools.js
@@ -3,25 +3,32 @@ var NlpToolsCtrl = function($scope, $http) {
   $scope.model = { }
 
   $scope.working = true;
-  $http.get("/api/tools", $scope.model.dependencies)
+  $http.get("/api/tools")
     .success(function(data, status, headers, config) {
       $scope.working = false;
       $scope.model.tools = data;
+
+      data.forEach(function (tool) {
+        $scope.model.toolInfo = {};
+        $http.get("/api/tools/" + tool).success(function(data, status, headers, config) {
+          $scope.model.toolInfo[tool] = data;
+        });
+      });
     })
     .error(function(data, status, headers, config) {
       $scope.working = false;
       $scope.errorMessage = data;
     });
 
-  $scope.runTool = function(name) {
+  $scope.runTool = function(tool) {
     $scope.working = true;
-    $http.post("/api/tools/" + name, $scope.model[name])
+    $http.post("/api/tools/" + tool, $scope.model[tool])
       .success(function(data, status, headers, config) {
         $scope.working = false;
         $scope.errorMessage = undefined;
 
         $scope.response = {};
-        $scope.response[name] = data;
+        $scope.response[tool] = data;
       })
       .error(function(data, status, headers, config) {
         $scope.working = false;
@@ -29,6 +36,10 @@ var NlpToolsCtrl = function($scope, $http) {
 
         $scope.errorMessage = data;
       });
+  }
+
+  $scope.showExample = function(tool) {
+    $scope.model[tool] = $scope.model.toolInfo[tool].example;
   }
 }
 
