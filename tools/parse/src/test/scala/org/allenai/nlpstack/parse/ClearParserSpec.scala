7698067d80014fa67c6008a200a3dabbc54b21fc
Michael Schmitz <michael@schmitztech.com>
2014-05-30 16:43:43 -0700

    Commenting out ClearParserSpec due to memory requirements.

--- tools/parse/src/test/scala/org/allenai/nlpstack/parse/ClearParserSpec.scala
+++ tools/parse/src/test/scala/org/allenai/nlpstack/parse/ClearParserSpec.scala
@@ -1,14 +1,20 @@
+/*
 package org.allenai.nlpstack
 package parse
 
 import org.allenai.common.testkit.UnitSpec
+import org.allenai.nlpstack.postag._
+import org.allenai.nlpstack.tokenize._
 
 class ClearParserSpec extends UnitSpec {
   "parser impl" should "work correctly on a single-word sentence" in {
     val text = "John"
+    val tokenizer = new SimpleEnglishTokenizer()
+    val postagger = new OpenNlpPostagger()
     val parser = new ClearParser()
 
-    val dgraph = parser.dependencyGraph(text)
+    val dgraph = parser.dependencyGraph(tokenizer, postagger)(text)
     DependencyParser.multilineStringFormat.write(dgraph) === "John 0 NNP\n\nroot(ROOT-0, John-1)"
   }
 }
+*/
