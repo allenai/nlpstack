1fea2eaf816092acef7935ed5eb975a8120c2b0a
Dirk Groeneveld <dirkg@allenai.org>
2014-06-18 13:52:08 -0700

    Added the Factorie tokenizer

---
+++ tools/tokenize/src/main/scala/org/allenai/nlpstack/tokenize/FactorieTokenizer.scala
@@ -0,0 +1,27 @@
+package org.allenai.nlpstack
+package tokenize
+
+import cc.factorie.app.nlp._
+import cc.factorie.app.nlp.segment.DeterministicTokenizer
+
+class FactorieTokenizer extends Tokenizer {
+  private val tokenizer = DeterministicTokenizer
+  private val map = new MutableDocumentAnnotatorMap ++=
+    DocumentAnnotatorPipeline.defaultDocumentAnnotationMap
+  map += tokenizer
+  private val pipeline = DocumentAnnotatorPipeline(
+    map = map.toMap,
+    prereqs = Nil,
+    tokenizer.postAttrs)
+
+  def tokenize(sentence: String): Seq[Token] = {
+    val doc = pipeline.process(new Document(sentence))
+
+    for (section <- doc.sections; token <- section.tokens)
+      yield Token(token.string, token.stringStart)
+  }
+}
+
+object FactorieTokenizerMain extends TokenizerMain {
+  val tokenizer = new FactorieTokenizer()
+}
