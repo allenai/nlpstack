ef6835ba10cc82ca59ebe46d878d708fcab8adf3
Dirk Groeneveld <dirkg@allenai.org>
2014-06-18 11:09:51 -0700

    Reformatted dependency exclusions

--- project/Build.scala
+++ project/Build.scala
@@ -16,8 +16,12 @@ object NlpstackBuild extends Build {
   val clearGroup = "com.clearnlp"
   val clearVersion = "2.0.2"
   val clear = clearGroup % "clearnlp" % clearVersion
-  val opennlp = "org.apache.opennlp" % "opennlp-tools" % "1.5.3" exclude("net.sf.jwordnet", "jwnl")
-  val chalk = "org.scalanlp" % "chalk" % "1.3.0" exclude("com.typesafe.akka", "akka-actor_2.10") exclude("org.apache.logging.log4j", "log4j-api") exclude("junit", "junit")
+  val opennlp = ("org.apache.opennlp" % "opennlp-tools" % "1.5.3" 
+    exclude("net.sf.jwordnet", "jwnl"))
+  val chalk = ("org.scalanlp" % "chalk" % "1.3.0"
+    exclude("com.typesafe.akka", "akka-actor_2.10")
+    exclude("org.apache.logging.log4j", "log4j-api")
+    exclude("junit", "junit"))    // why is this a dependency?
 
   val testingLibraries = Seq(allenAiTestkit % "test")
 
