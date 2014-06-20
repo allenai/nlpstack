218eb6b34727e627f69d3418087d113b7e9bff6e
Dirk Groeneveld <dirkg@allenai.org>
2014-06-20 11:00:12 -0700

    Got rid of `type SimpleEnglishTokenizer`

---
+++ tools/tokenize/src/main/scala/org/allenai/nlpstack/tokenize/SimpleEnglishTokenizer.scala
@@ -0,0 +1,4 @@
+package org.allenai.nlpstack.tokenize
+
+@deprecated("Please use defaultTokenizer instead", "2014-06-19")
+class SimpleEnglishTokenizer extends FactorieTokenizer
--- tools/tokenize/src/main/scala/org/allenai/nlpstack/tokenize/package.scala
+++ tools/tokenize/src/main/scala/org/allenai/nlpstack/tokenize/package.scala
@@ -1,8 +1,5 @@
 package org.allenai.nlpstack
 
 package object tokenize {
-  @deprecated("Please use defaultTokenizer instead", "2014-06-19")
-  type SimpleEnglishTokenizer = FactorieTokenizer
-
   def defaultTokenizer: Tokenizer = new FactorieTokenizer
 }
