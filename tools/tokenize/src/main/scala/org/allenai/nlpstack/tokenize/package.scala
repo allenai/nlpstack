36b2418ae3d0cb4aed8df690ab9e9aed7e34e049
Michael Schmitz <michael@schmitztech.com>
2014-06-19 19:51:29 -0700

    Remove tokenize package object.

--- tools/tokenize/src/main/scala/org/allenai/nlpstack/tokenize/package.scala
+++
@@ -1,7 +0,0 @@
-package org.allenai.nlpstack
-
-package object tokenize {
-  // Other places depend on SimpleEnglishTokenizer to exist, so for now we give
-  // it to them this way.
-  type SimpleEnglishTokenizer = FactorieTokenizer
-}
