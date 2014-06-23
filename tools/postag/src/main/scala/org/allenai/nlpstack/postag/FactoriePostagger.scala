b254c17b501ec831f675e9e69dab8f50ae590d69
Dirk Groeneveld <dirkg@allenai.org>
2014-06-23 15:09:17 -0700

    Simplified FactoriePostagger

--- tools/postag/src/main/scala/org/allenai/nlpstack/postag/FactoriePostagger.scala
+++ tools/postag/src/main/scala/org/allenai/nlpstack/postag/FactoriePostagger.scala
@@ -6,20 +6,7 @@ import cc.factorie.app.nlp.segment.DeterministicTokenizer
 import org.allenai.nlpstack.tokenize.Token
 
 class FactoriePostagger extends Postagger {
-  // We need to add a tokenizer manually because the default one that factorie
-  // uses splits hyphenated words, and we don't like that.
-  // Not sure why Factorie uses a tokenizer for this at all.
-  private val tokenizer =
-    new DeterministicTokenizer(tokenizeAllDashedWords = true)
   private val tagger = OntonotesForwardPosTagger
-  private val map = new MutableDocumentAnnotatorMap ++=
-    DocumentAnnotatorPipeline.defaultDocumentAnnotationMap
-  map += tokenizer
-  map += tagger
-  private val pipeline = DocumentAnnotatorPipeline(
-    map = map.toMap,
-    prereqs = Nil,
-    tagger.postAttrs)
 
   override def postagTokenized(tokens: Seq[Token]): Seq[PostaggedToken] = {
     // translate the tokens into a Factorie document
@@ -35,9 +22,9 @@ class FactoriePostagger extends Postagger {
       t => new cc.factorie.app.nlp.Token(t.offset, t.offset + t.string.length))
     section ++= factorieTokens
 
-    val doc = pipeline.process(factorieDoc)
+    tagger.predict(factorieTokens)  // modifies factoryTokens
 
-    for (section <- doc.sections; token <- section.tokens)
+    for (token <- factorieTokens)
       yield PostaggedToken(
       tagger.tokenAnnotationString(token),
       token.string,
