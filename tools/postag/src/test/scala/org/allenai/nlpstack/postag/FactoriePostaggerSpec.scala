93aa86e3e52ac82b1214ccd8ad4b97bd836c07e5
Dirk Groeneveld <dirkg@allenai.org>
2014-06-23 14:56:23 -0700

    Consolidated tests for the postaggers

--- tools/postag/src/test/scala/org/allenai/nlpstack/postag/FactoriePostaggerSpec.scala
+++ tools/postag/src/test/scala/org/allenai/nlpstack/postag/FactoriePostaggerSpec.scala
@@ -3,13 +3,6 @@ package org.allenai.nlpstack.postag
 import org.allenai.common.testkit.UnitSpec
 import org.allenai.nlpstack.tokenize._
 
-class FactoriePostaggerSpec extends UnitSpec {
-  "Factorie pos-tagger" should "correctly postag an example sentence" in {
-    val text = "This is a test of the OpenNlp postagger."
-    val tokenizer = defaultTokenizer
-    val postagger = new FactoriePostagger
-
-    val postagged = postagger.postag(tokenizer)(text)
-    assert(postagged.mkString("; ") === "This 0 DT; is 5 VBZ; a 8 DT; test 10 NN; of 15 IN; the 18 DT; OpenNlp 22 NNP; postagger 30 NN; . 39 .")
-  }
+class FactoriePostaggerSpec extends PostaggerSpec {
+  val taggerToTest = new FactoriePostagger
 }
