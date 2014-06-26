45f05473f83edd8636ae39ff7a80c5580df140af
Dirk Groeneveld <dirkg@allenai.org>
2014-06-25 17:58:20 -0700

    Adds test for FactorieParser

---
+++ tools/parse/src/test/scala/org/allenai/nlpstack/parse/FactorieParserSpec.scala
@@ -0,0 +1,33 @@
+package org.allenai.nlpstack.parse
+
+import org.allenai.common.testkit.UnitSpec
+import org.allenai.nlpstack.parse.graph.DependencyGraph
+import org.allenai.nlpstack.tokenize.defaultTokenizer
+import org.allenai.nlpstack.postag.defaultPostagger
+
+class FactorieParserSpec extends UnitSpec {
+  "FactorieParser" should "correctly parse a simple sentence" in {
+    val text = "A waffle is like a pancake with a syrup trap."
+
+    val tokens = defaultTokenizer.tokenize(text)
+    val postaggedTokens = defaultPostagger.postagTokenized(tokens)
+
+    val parser = new FactorieParser
+    val parseTree = parser.dependencyGraphPostagged(postaggedTokens)
+
+    val parseTreeStr = DependencyGraph.multilineStringFormat.write(parseTree)
+    val expectedParseTreeStr =
+      """|ParseTreeLabel(det)(waffle-2, A-1)
+        |ParseTreeLabel(nsubj)(is-3, waffle-2)
+        |root(ROOT-0, is-3)
+        |ParseTreeLabel(prep)(is-3, like-4)
+        |ParseTreeLabel(det)(pancake-6, a-5)
+        |ParseTreeLabel(pobj)(like-4, pancake-6)
+        |ParseTreeLabel(prep)(pancake-6, with-7)
+        |ParseTreeLabel(det)(trap-10, a-8)
+        |ParseTreeLabel(nn)(trap-10, syrup-9)
+        |ParseTreeLabel(pobj)(with-7, trap-10)
+        |ParseTreeLabel(punct)(is-3, .-11)""".stripMargin
+    assert(parseTreeStr === expectedParseTreeStr)
+  }
+}
