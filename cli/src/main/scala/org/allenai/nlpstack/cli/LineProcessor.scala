48226d94dee992e915ba9316d819c426b25c3355
Dirk Groeneveld <dirkg@allenai.org>
2014-07-28 17:35:23 -0700

    Formatting change

--- cli/src/main/scala/org/allenai/nlpstack/cli/LineProcessor.scala
+++ cli/src/main/scala/org/allenai/nlpstack/cli/LineProcessor.scala
@@ -100,9 +100,9 @@ abstract class LineProcessor(name: String) {
     System.err.println(f"${duration.toUnit(SECONDS)}%1.2f s")
 
     // Don't close stdin or stdout. It messes up the sbt REPL if you do.
-    if(config.inputFile.isDefined)
+    if (config.inputFile.isDefined)
       source.close()
-    if(config.outputFile.isDefined)
+    if (config.outputFile.isDefined)
       writer.close()
   }
 }
