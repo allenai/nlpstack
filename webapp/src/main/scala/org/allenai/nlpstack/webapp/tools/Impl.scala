fc253c252485a13a43b1bfb4ff968e597954f228
Dirk Groeneveld <dirkg@allenai.org>
2014-08-01 11:08:46 -0700

    Un-lazified some of the web demo

--- webapp/src/main/scala/org/allenai/nlpstack/webapp/tools/Impl.scala
+++ webapp/src/main/scala/org/allenai/nlpstack/webapp/tools/Impl.scala
@@ -12,10 +12,10 @@ object Impl {
   private[tools] val sentenceSegmenter = defaultSegmenter
   private[tools] val tokenizer = defaultTokenizer
   private[tools] val lemmatizer = new MorphaStemmer()
-  private[tools] lazy val postagger = defaultPostagger
+  private[tools] val postagger = defaultPostagger
   private[tools] val chunker = new OpenNlpChunker()
-  private[tools] lazy val dependencyParser = new PolytreeParser
-  private[tools] lazy val coref = defaultCorefResolver
+  private[tools] val dependencyParser = new PolytreeParser
+  private[tools] val coref = defaultCorefResolver
 
   val obamaText = "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University and Harvard Law School, where he served as president of the Harvard Law Review. He was a community organizer in Chicago before earning his law degree. He worked as a civil rights attorney and taught constitutional law at the University of Chicago Law School from 1992 to 2004. He served three terms representing the 13th District in the Illinois Senate from 1997 to 2004, running unsuccessfully for the United States House of Representatives in 2000."
   val obamaSentences = sentenceSegmenter(obamaText) map (_.text) mkString "\n"
