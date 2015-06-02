a369c22b401fa4eca8ed81a8de296a75d8c123ac
Satwant Rana <i-satwantr@allenai.org>
2015-06-02 11:01:09 -0700

    Solved concurrency issues

--- tools/chunk/src/main/scala/org/allenai/nlpstack/chunk/OpenNlpChunker.scala
+++ tools/chunk/src/main/scala/org/allenai/nlpstack/chunk/OpenNlpChunker.scala
@@ -1,12 +1,15 @@
 package org.allenai.nlpstack.chunk
 
 import org.allenai.common.Resource
-import org.allenai.nlpstack.core.{ChunkedToken, Chunker, PostaggedToken}
+import org.allenai.nlpstack.core.{ ChunkedToken, Chunker, PostaggedToken }
 
-import opennlp.tools.chunker.{ChunkerME, ChunkerModel}
+import opennlp.tools.chunker.{ ChunkerME, ChunkerModel }
 
 class OpenNlpChunker extends Chunker {
   private val chunker = new ChunkerME(OpenNlpChunker.model)
+  private final val chunker_t: ThreadLocal[ChunkerME] = new ThreadLocal[ChunkerME]() {
+    override protected def initialValue(): ChunkerME = new ChunkerME(OpenNlpChunker.model)
+  }
 
   def chunkPostaggedOriginal(tokens: Seq[PostaggedToken]): Seq[ChunkedToken] = {
     // OpenNLP uses : as the postag for hyphens, but we use HYPH, so we change it back before
@@ -15,21 +18,23 @@ class OpenNlpChunker extends Chunker {
       if (t.string == "-") PostaggedToken(t, ":") else t
     }
 
-    val chunks = chunker.chunk(tokens.map(_.string).toArray, fixedTokens.map(_.postag).toArray)
+    val chunks = chunker_t.get().chunk(tokens.map(_.string).toArray, fixedTokens.map(_.postag)
+      .toArray)
     (tokens zip chunks) map { case (token, chunk) => ChunkedToken(token, chunk) }
   }
 
   //Open NLP runs into random NullPointerExceptions. The following is a workaround that reduces the
   // probability of running into errors
-  def chunkPostagged(tokens: Seq[PostaggedToken]): Seq[ChunkedToken] = {
-    def helper(tries: Int): Seq[ChunkedToken] = try {
-      if (tries == 0) throw java.lang.NullPointerException
-      else chunkPostaggedOriginal(tokens)
-    } catch {
-      case e: Throwable => helper(tries - 1)
-    }
-    helper(3)
-  }
+//  def chunkPostagged(tokens: Seq[PostaggedToken]): Seq[ChunkedToken] = {
+//    def helper(tries: Int): Seq[ChunkedToken] = try {
+//      if (tries == 0) throw new java.lang.NullPointerException()
+//      else chunkPostaggedOriginal(tokens)
+//    } catch {
+//      case e: Throwable => helper(tries - 1)
+//    }
+//    helper(0)
+//  }
+  def chunkPostagged(tokens: Seq[PostaggedToken]): Seq[ChunkedToken] = chunkPostaggedOriginal(tokens)
 }
 
 object OpenNlpChunker {
